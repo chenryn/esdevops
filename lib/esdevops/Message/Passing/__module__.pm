6c46f0206ed613195ec51b60b398916b6b51e77d
chenryn <rao.chenlin@gmail.com>
2013-07-26 18:47:28 +0800

    修改github为只读地址

--- lib/esdevops/Message/Passing/__module__.pm
+++ lib/esdevops/Message/Passing/__module__.pm
@@ -7,7 +7,7 @@ desc "get Message::Passing from github and installed by cpanm";
 task "get_mp", group => 'servers', sub {
    install package => "cpanminus";
    set repository => 'utils',
-      url => 'git@github.com:chenryn/Message-Passing-Utils.git';
+      url => 'https://github.com/chenryn/Message-Passing-Utils.git';
    checkout 'utils',
       path => 'mp-utils';
    run "cpanm ./$_" for glob("mp-utils/Message*");
